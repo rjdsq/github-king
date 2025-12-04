c68e5b9e917038bdae33209e2765b36be5a8820c
牡丹君 <49619325+rjdsq@users.noreply.github.com>
2025-12-04 23:33:44 +0800

    Update 光圈.js

--- js/2点击/光圈.js
+++ js/2点击/光圈.js
@@ -23,7 +23,7 @@ style.textContent = `
     width: 60px;
     height: 60px;
     border-radius: 50%;
-    border: 2px solid transparent;
+    border: 4px solid transparent;
     animation: clickEffectAnimation 0.7s ease-out;
     pointer-events: none;
     box-sizing: border-box;
